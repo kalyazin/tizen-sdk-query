74f4e6c8e5a6a041b4fd8d31d8e2d101813254c7
Nikita Kalyazin <nkalyazin@gmail.com>
2014-06-29 01:06:43 +0400

    fix: strict dep check

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -20,7 +20,7 @@ get_deps () {
 	local _pn="$1"
 	sed -n '/Package: '${_pn}'/,/^$/p' ${info_dir}/${meta_list_file} \
 		| head -n -1 \
-		| awk -F ':' '/Install/{ print $2 }' \
+		| awk -F ':' '/Install-dependency: /{ print $2 }' \
 		| tr -d ' ' \
 		| awk -F ',' '{ for (i=1; i<=NF; i++) print $i }' \
 		| sort
